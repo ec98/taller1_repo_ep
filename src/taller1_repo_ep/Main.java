f6f327c87379cde7669e85776632e81728d5c3ca
Carlos <Carlos@CARLOS>
2022-05-17 20:57:47 -0500

    Segundo cambio solicitado

--- src/taller1_repo_ep/Main.java
+++ src/taller1_repo_ep/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Mi primer proyecto JAVA en GitHub");
-		
+		System.out.println("Mi segundo proyecto JAVA en GitHub");
 	}
 }
